1e53ae260339bed0d9a2532d07298fc1f268cf77
NEVIN <nevin@pop-os.localdomain>
2023-08-21 04:01:50 +0530

    register

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,80 +1,110 @@
-import { StyleSheet, View } from 'react-native'
-import React, { useLayoutEffect, useState } from 'react'
-import { KeyboardAvoidingView } from 'react-native'
-import { StatusBar } from 'expo-status-bar'
-import { Button,Input , Image,Text } from 'react-native-elements';
-import { auth } from '../firebase';
+import React, { useLayoutEffect, useState } from 'react';
+import { KeyboardAvoidingView } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Button, Input, Text } from 'react-native-elements';
+import axios from 'axios';
+import { StyleSheet, View } from 'react-native';
 
-const RegisterScreen = ({navigation}) => {
-    const [name, setName] = useState("");
-    const [email,setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [imageUrl, setImageUrl] = useState("");
+const RegisterScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-          headerBackTitle: "Login",
-        });
-      }, [navigation]);
-
-      const register = () => {
-        auth
-          .createUserWithEmailAndPassword(email, password)
-          .then((authUser) => {
-            authUser.user
-              .updateProfile({
-                displayName: name,
-                photoURL:
-                  imageUrl ||
-                  "https://as1.ftcdn.net/v2/jpg/05/16/27/58/1000_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg",
-              })
-              .then(() => {
-                // Profile update successful
-                // Redirect or perform any other actions
-                navigation.replace('ForbiddenForum');
-              })
-              .catch((error) => {
-                // Error updating profile
-                console.log(error);
-              });
-          })
-          .catch((error) => alert(error.message));
+  // Replace useNavigate with navigation.navigate
+  // Also, the function name should match the one in the onPress event
+  const register = async () => {
+    try {
+      const data = {
+        email: email,
+        password: password,
+        confirm_Password: confirmPassword,
       };
+
+      // Use axios for the fetch request
+      const response = await axios.post(
+        'https://glupingtest.onrender.com/docs#/Users/create_user_users__post',
+        data,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (response.status === 201) {
+        // User registered successfully
+        const result = response.data;
+        navigation.navigate('Sinister Entryway');
+      } else if (response.status === 422) {
+        // Validation error
+        const errorData = response.data;
+        // Handle validation errors here
+      } 
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerBackTitle: 'Sinister Entryway',
+    });
+  }, [navigation]);
+
   return (
     <KeyboardAvoidingView behavior="padding" style={styles.container}>
-      <StatusBar style='light'/>
-      <Text h3 style={{ marginBottom: 50}}>
-        Summon Your Identity      
+      <StatusBar style="light" />
+      <Text h3 style={{ marginBottom: 50 }}>
+        Summon Your Identity
       </Text>
       <View style={styles.inputContainer}>
-        <Input placeholder="Full name" autofocus type='text' value={name} onChangeText={(text) => setName(text)} />
-        <Input placeholder="Email"  type='Email' value={email} onChangeText={(text) => setEmail(text)} />
-        <Input placeholder="Password" secureTextEntry type='password' value={password} onChangeText={(text) => setPassword(text)} />
-        <Input placeholder="Profile pic url(venonkii)" autofocus type='text' value={imageUrl} onChangeText={(text) => setImageUrl(text)} onSubmitEditing={register} />
+        <Input
+          placeholder="Email"
+          type="email" // Use lowercase 'email' instead of 'Email'
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <Input
+          placeholder="Confirm Password"
+          secureTextEntry
+          type="password"
+          value={confirmPassword}
+          onChangeText={(text) => setConfirmPassword(text)}
+        />
       </View>
-      <Button  containerStyle={styles.button} raised onPress={register} title="Register" />
-      <View style={{height:100}} />
+      {/* Use the 'register' function in the onPress event */}
+      <Button
+        containerStyle={styles.button}
+        raised
+        onPress={register}
+        title="Register"
+      />
+      <View style={{ height: 100 }} />
     </KeyboardAvoidingView>
-  )
-}
+  );
+};
 
-export default RegisterScreen
+export default RegisterScreen;
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems:"center",
-        justifyContent:"center",
-        padding:10,
-        backgroundColor:"white",
-    },
-    button:{
-        width:200,
-        marginTop:10,
-
-    },
-    inputContainer:{
-        width:300,
-    },
-
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+  inputContainer: {
+    width: 300,
+  },
 });
